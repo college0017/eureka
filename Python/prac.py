50535c6f97fe5c63e503017b4b7436347e7e1472
college0017 <103097891+college0017@users.noreply.github.com>
2022-04-13 12:53:05 +0530

    Add files via upload

---
+++ Python/prac.py
@@ -0,0 +1,6 @@
+name='Atharv Khorjuvenkar'
+age=18
+college="Dm's college and research center"
+print("My name is",name,"my age is",age)
+print("I study is",college)
+
---
+++ Python/var.py
@@ -0,0 +1,16 @@
+"""
+Program to Print......
+Sum
+Division
+Multipication
+Modulus
+Subtaction
+"""
+a=10
+b=5
+
+print('sum=',a+b)
+print('sub=',a-b)
+print('product=',a*b)
+print('mod=',a%b)
+print('divide=',a/b)
